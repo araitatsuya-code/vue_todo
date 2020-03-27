ef8e5dc74becbd27137bb4ac4c28768d635974d5
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-03-27 21:56:32 +0900

    coin_with_axios
    bind(this)でvueインスタンスを指定

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 var app = new Vue({
 	el: '#app',
-	data:{
-  	newItem: '',
-    todos: []
+  date: {
+  	bpi: null
   },
-  methods: {
-  	addItem: function(event){
-    	//alert();
-      if(this.newItem == '') return;
-      var todo = {
-      	item: this.newItem,
-        isDone: false
-      };
-      this.todos.push(todo);
-      this.newItem = '';
-    },
-    deleteItem: function(index){
-    this.todos.splice(index,1)
-    }
+  mounted: function(){
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+			this.bpi = response.data.bpi
+}.bind(this))
+.catch(function(error){
+  console.log(error)
+})
   }
 })
