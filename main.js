105b2cda6a337bf857d468ae86c784ae63e13c49
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-03-27 19:52:00 +0900

    todo_add_css

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js'
+var app = new Vue({
+	el: '#app',
+	data:{
+  	newItem: '',
+    todos: []
+  },
+  methods: {
+  	addItem: function(event){
+    	//alert();
+      if(this.newItem == '') return;
+      var todo = {
+      	item: this.newItem,
+        isDone: false
+      };
+      this.todos.push(todo);
+      this.newItem = '';
+    }
   }
 })
